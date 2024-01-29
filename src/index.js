042b06317b9201b2932e0f4210c9c620a7e95414
denis <denis@MacBook-Pro-denis.local>
2024-01-29 16:16:31 +0200

    Error correction thesis submission

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom"
 import * as ReactDOM from "react-dom/client";
-import App from "../src/components/App/App";
+import App from "./components/App/App";
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
